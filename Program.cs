6d292360eb45268daaf6e1d7d95206b7d5f79c7e
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-03-10 15:21:23 +0300

    add new str array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] array = new string[] { "hello", "2", "world", ":-)" };
+
