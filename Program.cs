60e7cb95c6d78bb7bffcf3f9cc740b31123dbbcf
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-03-10 15:47:03 +0300

    add new method ResultArray

--- Program.cs
+++ Program.cs
@@ -1,19 +1,42 @@
 ﻿Console.Clear();
 
+// Создаем новый массив
 string[] array = new string[] { "hello", "2", "world", ":-)" };
-
-int GetArray(string[] array) // функция по подсчет кол-ва элементов в массиве 
+// Создаем функцию 
+int GetArray(string[] array) // проверка на кол-во элементов в массиве и соответсвие условиям задачи <= 3
 {
-    int count = 0;
+    int count = 0; // задаем счетчик
+
+    for (int i = 0; i < array.Length; i++) //условие выхода
+    {
+        if (array[i].Length <= 3)
+        {
+            count += 1; // count = count + 1
+        }
+    }
+    return count; // вернем значение count 
+}
+
+// Console.WriteLine(GetArray(array)); // проверка работы функции
+
+void ResultArray(int count) // в функци resultArray создается новый массив, который заполняется
+// значениями, удовлетворяющими условия задачи.
+{
+    string[] newArray = new string[count];
+
+    int j = 0; // задаем счетчик
 
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            count += 1;
+            newArray[j] = array[i];
+            j += 1; // j = j + 1
         }
     }
-    return count;
+    var rolting = string.Join(" ", newArray); // раздеяем пробелом элементы
+    Console.Write(rolting);
 }
 
-Console.WriteLine(GetArray(array)); // проверка работы функции
+ResultArray(GetArray(array)); //запуск программы
+
