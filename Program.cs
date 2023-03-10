9744a3000ce0ca6b0a2961a529c47e8ce5cb75a9
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-03-10 15:23:39 +0300

    add new method getArray

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 
 string[] array = new string[] { "hello", "2", "world", ":-)" };
 
+int GetArray(string[] array) // функция по подсчет кол-ва элементов в массиве 
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine(GetArray(array)); // проверка работы функции
